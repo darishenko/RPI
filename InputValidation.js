37326ba370785cff7365b3fbb35ed0d1dfe3ea00
darishenko <89320281+darishenko@users.noreply.github.com>
2022-05-13 22:24:50 +0300

    Add files via upload
    Add a JS file for form with field validation "formForSendMessage" (Contact)

---
+++ InputValidation.js
@@ -0,0 +1,71 @@
+'use strict'
+const TIME_FOR_SEND_FORM = 5000;
+const TEXT_FOR_SEND_FORM = "The form has been submitted";
+document.addEventListener("DOMContentLoaded", function() {
+
+    const form = document.getElementById("formForSendMessage");
+    form.addEventListener('submit', formSend);
+    const buttonToSendForm = document.getElementById("contact-BtnForSendTOEmail");
+
+    const inputToCheck = form.querySelectorAll('._req');
+    for(let index = 0; index<inputToCheck.length; index++){
+        inputToCheck[index].oninput  = function () {
+            CheckInputForFormButton();
+        }
+    }
+
+    async function formSend(e){
+        e.preventDefault();
+        if (buttonToSendForm.disabled === false ){
+            buttonToSendForm.disabled =true;
+            //send form
+            form.reset();
+            create();
+        }
+    }
+
+    function CheckInputForFormButton (){
+        let error = 0;
+
+        for(let index = 0; index<inputToCheck.length; index++){
+            const input = inputToCheck[index];
+
+            if (input.value.trim() === '') {
+                error++;
+            }else {
+                if(input.classList.contains("_email")) {
+                    if (!emailCheck(input)) {
+                        error++;
+                    }
+                }
+
+                if(input.classList.contains("_name")) {
+                    if (!nameCheck(input)) {
+                        error++;
+                    }
+                }
+            }
+        }
+
+        buttonToSendForm.disabled = error !== 0;
+    }
+
+    function emailCheck(input){
+        return /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,8})+$/.test(input.value.trim());
+    }
+
+    function nameCheck(input){
+        return /^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$/.test(input.value.trim());
+    }
+
+    function create() {
+        let divFormMessage = document.createElement('div');
+        divFormMessage.classList.add("sendMessage");
+        divFormMessage.textContent= TEXT_FOR_SEND_FORM;
+        document.body.append(divFormMessage);
+
+        setTimeout(function () {
+            divFormMessage.remove();
+        }, TIME_FOR_SEND_FORM)
+    }
+});
